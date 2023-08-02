66d528429b628b3e3dc742ef854c59c9cdfd3181
sdztrk <msaidozturk1@gmail.com>
2023-08-02 14:06:35 +0200

    x and y axixs been changed

--- solution/app.js
+++ solution/app.js
@@ -7,7 +7,7 @@ const img = document.querySelector(".container img")
 container.addEventListener("mouseover", (e) => {
     let xAxis = (window.innerWidth/2 -e.pageX)/10
     let yAxis = (window.innerHeight/2-e.pageY)/10
-    card.style.transform = `rotateY( ${yAxis}deg) rotateX( ${xAxis}deg )`;
+    card.style.transform = `rotateY( ${xAxis}deg) rotateX( ${yAxis}deg )`;
     img.style.transform = `scale(1.1)`
     // img.style.transform = `rotateY( ${xAxis}deg) rotateX( ${yAxis}deg )`;
 })
