9c24e58cd6c6b34351d0f3397b5522f68a510a05
sdztrk <msaidozturk1@gmail.com>
2023-08-02 03:49:44 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+//Selecting the elements
+const container = document.querySelector(".container")
+const card = document.querySelector(".card")
+const img = document.querySelector(".container img")
+
+//Moving animation event
+container.addEventListener("mouseover", (e) => {
+    let xAxis = (window.innerWidth/2 -e.pageX)/10
+    let yAxis = (window.innerHeight/2-e.pageY)/10
+    card.style.transform = `rotateY( ${yAxis}deg) rotateX( ${xAxis}deg )`;
+    img.style.transform = `scale(1.1)`
+    // img.style.transform = `rotateY( ${xAxis}deg) rotateX( ${yAxis}deg )`;
+})
+
+//Animate out
+container.addEventListener("mouseout",() => {
+    card.style.transform= `rotateY(0deg) rotateX(0deg)`
+    img.style.transform= `scale(1)`
+})
